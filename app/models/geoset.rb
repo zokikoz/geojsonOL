e76a0eae75a4d94ff5b3683e8ce5963e1b1896ba
zokikoz <77430357+zokikoz@users.noreply.github.com>
2021-05-25 14:12:37 +0800

    Added JSON file upload to DB

--- app/models/geoset.rb
+++ app/models/geoset.rb
@@ -1,3 +1,14 @@
 class Geoset < ApplicationRecord
+  # Should be before has_one_attached to find file, because _after callbacks run in reverse order
+  after_commit :json_upload, on: [:create, :update], if: -> { geojson_file.attached? }
+
   has_one_attached :geojson_file
+
+  private
+
+  def json_upload
+    json = JSON.parse(geojson_file.download)
+    # Using direct UPDATE SQL via update_column method to avoid infinite loop on callback
+    update_column(:geojson, json)
+  end
 end
