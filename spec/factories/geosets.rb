694e5ea65b644318479f7a644bd433d29ffe4012
zokikoz <77430357+zokikoz@users.noreply.github.com>
2021-05-29 19:08:20 +0800

    Tests styling

--- spec/factories/geosets.rb
+++ spec/factories/geosets.rb
@@ -2,6 +2,11 @@ FactoryBot.define do
   factory :geoset do
     id { 1 }
     name { 'GeoJSON polygon' }
-    geojson { '{"type":"FeatureCollection","features":[{"type":"Feature","properties":{},"geometry":{"type":"Polygon","coordinates":[[[103.0078125,50.84757295365389],[111.26953125,50.84757295365389],[111.26953125,56.072035471800866],[103.0078125,56.072035471800866],[103.0078125,50.84757295365389]]]}}]}' }
+    geojson { '{"type":"FeatureCollection","features":[]}' }
+
+    trait :with_file do
+      geojson_file = File.open(Rails.root.join('spec', 'fixtures', 'map.geojson'))
+      Rack::Test::UploadedFile.new(geojson_file, 'application/geo+json', true, original_filename: 'map.geojson')
+    end
   end
 end
--- spec/models/geoset_spec.rb
+++ spec/models/geoset_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-fixture = Rails.root.join('spec', 'fixtures', 'map.geojson')
 
 RSpec.describe Geoset, type: :model do
   describe "Presence" do
@@ -13,8 +12,10 @@ RSpec.describe Geoset, type: :model do
 
   describe "File upload" do
     subject { FactoryBot.create(:geoset) }
+
     context "with valid file" do
-      json = JSON.parse(File.read(fixture))
+      fixture = Rails.root.join('spec', 'fixtures', 'map.geojson')
+      fixture_json = JSON.parse(File.read(fixture))
       before do
         subject.geojson_file.attach(io: File.open(fixture), filename: 'map.geojson', content_type: 'application/geo+json')
       end
@@ -22,14 +23,18 @@ RSpec.describe Geoset, type: :model do
         expect(subject.geojson_file).not_to be_attached
       end
       it "load file to database" do
-        expect(subject.geojson).to eq(json)
+        expect(subject.geojson).to eq(fixture_json)
       end
     end
 
-    it "does not process invalid file" do
-      subject.geojson_file.attach({ io: StringIO.new('Test'), filename: 'test.gif', content_type: 'image/gif' })
-      subject.validate
-      expect(subject.errors[:geojson_file]).to include('invalid content')
+    context "with invalid file" do
+      before do
+        subject.geojson_file.attach(io: StringIO.new('Test'), filename: 'test.gif', content_type: 'image/gif')
+        subject.validate
+      end
+      it "does not process invalid file" do
+        expect(subject.errors[:geojson_file]).to include('invalid content')
+      end
     end
   end
 end
